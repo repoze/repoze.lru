f2c531752cdb363b318cdce77c17157412491a75
Tres Seaver <tseaver@palladion.com>
2016-11-12 17:21:15 -0500

    Get next version right.
    Add explicit support for Python 3.4, 3.5 and PyPy3.
    
    Drop support for Python 2.6 and 3.2.

--- setup.py
+++ setup.py
@@ -27,17 +27,18 @@ except:
 testing_extras = ['nose', 'coverage']
 
 setup(name='repoze.lru',
-      version='0.7.1',
+      version='0.7dev0',
       description='A tiny LRU cache implementation and decorator',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
           "Intended Audience :: Developers",
           "Programming Language :: Python",
-          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.2",
           "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: Implementation :: CPython",
           "Programming Language :: Python :: Implementation :: PyPy",
           "License :: Repoze Public License",
