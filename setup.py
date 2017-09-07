627bea5c14a4fe16e0169c6de5607cf225f8872b
Tres Seaver <tseaver@palladion.com>
2017-09-07 01:01:29 -0400

    svb

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except:
 testing_extras = ['nose', 'coverage']
 
 setup(name='repoze.lru',
-      version='0.7',
+      version='0.8dev0',
       description='A tiny LRU cache implementation and decorator',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
